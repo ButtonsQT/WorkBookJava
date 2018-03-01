54e357aba5c72e371b4810dab7288a18c9f16a34
Buttons_QT <36940700+ButtonsQT@users.noreply.github.com>
2018-02-28 21:49:20 -0600

    Add files via upload

---
+++ landCalculation.java
@@ -0,0 +1,27 @@
+/*
+One acre of land is equivalent to 43,560 square feet. Write a program that 
+calculates the number of acres in a tract of land with 389,767 square feet..
+ */
+
+/**
+ *
+ * @author findl
+ */
+public class landCalculation {
+    public static void main(String[] args){
+        int totalLand;
+        int acreOfLand;
+        int numberOfAcres; 
+        acreOfLand = 43560;
+        totalLand = 389767;
+        numberOfAcres = (totalLand / acreOfLand );
+                
+                System.out.print( numberOfAcres);
+        
+        
+       
+        
+        
+    }
+    
+}
